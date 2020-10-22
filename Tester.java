123b25dd4fdb08f23112c7c6f358f1cfd644a90e
lamdaham <ilam21@stuy.edu>
2020-10-22 15:55:20 -0400

    Added test cases to test magic methods

--- Tester.java
+++ Tester.java
@@ -2,14 +2,18 @@ import java.util.Arrays;
 
 public class Tester {
 	public static void main(String[] args) {
-		int[][]  A  =  {  {  1,  0, 12, -1 },
-                  {  7, -2,  2,  1 },
-                  { -5, -2,  2, -9 }
+		int[][]  A  =  {  {1,2,3,4},
+                  { 2,3,4,1},
+                  { 3,4,1,2}
                	};
 		int[]  B  =   {  1, 3, 5 };
-		int[][] C ={{1,3,4,5},
-					{2,4,5},
-					{2,7,3}}; 
-		System.out.println((ArrayOps.sum(A)));
+		int[][] C ={{1,1,1},
+					{2,2,2},
+					{3,3,3}}; 
+		int [][] D = { {  2,  2, 2 },
+   						{  2, 2, 2 } };
+   		int [][] E = {{  2,  4, 2 }, 
+						{  2, 2, 2 } };
+		System.out.println((ArrayOps.isLocationMagic(E,0,1)));
 	}
 }
