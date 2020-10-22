a93f7599e7dfa02ff1b625ab9bb234207d09a5b9
lamdaham <ilam21@stuy.edu>
2020-10-22 14:28:34 -0400

    Added largest() method

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,13 @@ public class ArrayOps {
 		}
 		return output;
 	}
+	public static int largest(int[]arr) {
+		int output = arr[0];
+		for (int i = 0; i<arr.length; i++) {
+			if (arr[i]>output) {
+				output = arr[i];
+			}
+		}
+		return output;
+	}
 }
